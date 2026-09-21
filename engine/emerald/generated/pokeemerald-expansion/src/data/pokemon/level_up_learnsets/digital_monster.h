f76992bba95172308e8c8a38db71a6228de81244
SakuraiTsubaki <nokang3@naver.com>
2026-09-22 05:13:21 +0900

    Generate Digital Monster gameplay-v0 learnsets

---
+++ engine/emerald/generated/pokeemerald-expansion/src/data/pokemon/level_up_learnsets/digital_monster.h
@@ -0,0 +1,7 @@
+#define LEVEL_UP_MOVE(lvl, moveLearned) {.move = moveLearned, .level = lvl}
+#define LEVEL_UP_END {.move = LEVEL_UP_MOVE_END, .level = 0}
+
+#include "digital_monster_part_1.h"
+#include "digital_monster_part_2.h"
+#include "digital_monster_part_3.h"
+#include "digital_monster_part_4.h"
