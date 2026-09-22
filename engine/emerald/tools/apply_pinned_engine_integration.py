d19b97c0d2dc950b0c8c46321b5f3d6de179d894
SakuraiTsubaki <nokang3@naver.com>
2026-09-22 09:15:57 +0900

    Disable battle-only overworld forms in Digital Monster profile

--- engine/emerald/tools/apply_pinned_engine_integration.py
+++ engine/emerald/tools/apply_pinned_engine_integration.py
@@ -264,7 +264,9 @@ def main() -> None:
     marker = "#define OW_POKEMON_OBJECT_EVENTS       TRUE       // Adds Object Event fields for every species. Can be used for NPCs using the OBJ_EVENT_GFX_SPECIES macro (eg. OBJ_EVENT_GFX_SPECIES(BULBASAUR))\n"
     override = """#ifdef DIGITAL_MONSTER_SERIES
 #undef OW_POKEMON_OBJECT_EVENTS
+#undef OW_BATTLE_ONLY_FORMS
 #define OW_POKEMON_OBJECT_EVENTS FALSE
+#define OW_BATTLE_ONLY_FORMS FALSE
 #endif
 """
     t = replace_once(t, marker, marker + override, "overworld species safety gate")
